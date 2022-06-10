45fde9e9283c6f8656be50c13ba1decddf5edaf8
woooferz <85282355+woooferz@users.noreply.github.com>
2022-06-10 20:33:20 +1000

    added json support

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template, redirect, jsonify, send_from_directory
 import yaml
+import json
 
-version = "v0.1"
+version = "v0.1.1"
 
 print(f"Skarf {version}")
 
 print("Loading Config...")
-with open("config/config.yml", "r") as f:
-    config = yaml.load(f.read(), Loader=yaml.Loader)
+try:
+    with open("config/config.yml", "r") as f:
+        config = yaml.load(f.read(), Loader=yaml.Loader)
+    print("Loaded YAML")
+except FileNotFoundError:
+    try:
+        with open("config/config.json", "r") as f:
+            config = json.loads(f.read())
+        print("Loaded JSON")
+    except FileNotFoundError:
+        print("Could not locate config!")
+        exit(1)
+
 print("Loaded!")
 # print(config)
 
