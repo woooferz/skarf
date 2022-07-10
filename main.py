7d6a0676e5b1fc1bbab2ae34b0449f67e396384d
woooferz <85282355+woooferz@users.noreply.github.com>
2022-07-10 11:16:22 +1000

    Fixed linter issues &  fixed example JSON for renderer version 3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, jsonify, send_from_directory
-import yaml
 import json
+
+import yaml
+from flask import Flask, render_template, send_from_directory
 
 skarf_version = "v0.2 alpha"
 
@@ -32,7 +33,7 @@ if config:
 else:
     print("Error while loading config")
 # print(config)
-if config['debug'] == True:
+if config["debug"] is True:
     print("SKARF IS IN DEBUG MODE! THIS IS NOT PRODUCTION READY!")
 
 app = Flask(__name__)
@@ -42,25 +43,30 @@ app = Flask(__name__)
 def home():
     global config
 
-    if config['debug'] == True:
+    if config["debug"] is True:
         print("Reloading Config! (THIS IS BECAUSE DEBUG MODE IS ON)")
         config = load_config()
         print("FINISHED RELOADING CONFIG")
 
     if config:
-        return render_template(f"versions/{str(config['version'])}.html", config=config['settings'], more=config, version=skarf_version)
+        return render_template(
+            f"versions/{str(config['version'])}.html",
+            config=config["settings"],
+            more=config,
+            version=skarf_version,
+        )
     else:
         return render_template("no_config.html", the_version=skarf_version)
 
 
-@app.route('/static/<path:path>')
+@app.route("/static/<path:path>")
 def send_static(path):
-    return send_from_directory('static/', path)
+    return send_from_directory("static/", path)
 
 
-@app.route('/res/<path:path>')
+@app.route("/res/<path:path>")
 def send_res(path):
-    return send_from_directory('res', path)
+    return send_from_directory("res", path)
 
 
 if __name__ == "__main__":
